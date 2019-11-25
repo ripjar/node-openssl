38f458180c47b0fa9a76cb35bb249596968debd8
James Moxon <james.moxon@ripjar.com>
2019-11-25 14:43:27 +0000

    rename to lib

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const modulus = require("../");
+const lib = require("../");
 const fs = require("fs");
 const assert = require("assert");
 
@@ -21,53 +21,53 @@ let result;
 
 // key must be string
 try {
-  result = modulus.RSAPrivateKey(key1);
+  result = lib.RSAPrivateKey(key1);
 } catch (e) {
   assert(e.message.indexOf("Failed to parse string") === 0);
 }
 
 // cert must be string
 try {
-  result = modulus.X509PublicKey(cert1);
+  result = lib.X509PublicKey(cert1);
 } catch (e) {
   assert(e.message.indexOf("Failed to parse string") === 0);
 }
 
 // check modulus of first cert matches modulus calculated using openssl command line
-result = modulus.X509PublicKey(cert1.toString());
+result = lib.X509PublicKey(cert1.toString());
 assert(result.n === mod1.toString());
 
 // check modulus of first key matches modulus calculated using openssl command line
-result = modulus.RSAPrivateKey(key1.toString());
+result = lib.RSAPrivateKey(key1.toString());
 assert(result.n === mod1.toString());
 
 // check modulus of second key matches modulus calculated using openssl command line
-result = modulus.RSAPrivateKey(key2.toString());
+result = lib.RSAPrivateKey(key2.toString());
 assert(result.n === mod2.toString());
 
 // check modulus of third cert matches modulus calculated using openssl command line
 // dont need to pass in passphrase here
-result = modulus.X509PublicKey(cert3.toString());
+result = lib.X509PublicKey(cert3.toString());
 assert(result.n === mod3.toString());
 
 // check modulus of third key matches modulus calculated using openssl command line
 // has passphrase, lets try without passing it in
 try {
-  result = modulus.RSAPrivateKey(key3.toString());
+  result = lib.RSAPrivateKey(key3.toString());
 } catch (e) {
   assert(false === true);
 }
 
 // check bad passphrase
 try {
-  result = modulus.RSAPrivateKey(key3.toString(), "badpassphrase");
+  result = lib.RSAPrivateKey(key3.toString(), "badpassphrase");
 } catch (e) {
   console.error(e);
   assert(true === false);
 }
 
 // check good passphrase
-result = modulus.RSAPrivateKey(key3.toString(), "r1pj4r");
+result = lib.RSAPrivateKey(key3.toString(), "r1pj4r");
 assert(true === false);
 
 console.log("Tests passed!");
