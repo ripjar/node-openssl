a9e37ba24c0a01bb5f602d5a1a835cc5fc8fad87
Joshua Houghton <joshua.houghton@ripjar.com>
2020-05-16 21:28:05 +0100

    fix rsa_st not being defined in node12

--- main.c
+++ main.c
@@ -23,6 +23,33 @@
 #include <openssl/bio.h>
 #include <openssl/x509.h>
 
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+
+struct rsa_st {
+    /*
+     * #legacy
+     * The first field is used to pickup errors where this is passed
+     * instead of an EVP_PKEY.  It is always zero.
+     * THIS MUST REMAIN THE FIRST FIELD.
+     */
+    int dummy_zero;
+
+    int32_t version;
+    const RSA_METHOD *meth;
+    /* functional reference if 'meth' is ENGINE-provided */
+    ENGINE *engine;
+    BIGNUM *n;
+    BIGNUM *e;
+    BIGNUM *d;
+    BIGNUM *p;
+    BIGNUM *q;
+    BIGNUM *dmp1;
+    BIGNUM *dmq1;
+    BIGNUM *iqmp;
+};
+
+#endif
+
 #include <string.h>
 
 static napi_value
